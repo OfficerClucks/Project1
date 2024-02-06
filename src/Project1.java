959f9e06abb30bfb9d34603d8a8a8bf933038cfe
OfficerClucks <97991336+OfficerClucks@users.noreply.github.com>
2024-02-06 13:31:15 -0600

    Add files via upload
    Uploading old class projects

---
+++ src/Project1.java
@@ -0,0 +1,15 @@
+
+public class Project1 {
+/** 
+ * This is my first Java program
+ * @author Ben Cruickshak
+ * @version 1.0
+ */
+	
+	public static void main(String[] args)
+	{
+		System.out.println("what do you call 8 rabbits?" 
+	+ " A rabbyte!");
+		
+	}
+}
